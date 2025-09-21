019ca3d2e055503d9bbeefa481a97f4090cfb303
Mehreen-Siddique <mehreensiddique50@gmail.com>
2025-09-21 06:40:08 +0500

    Add the text fields for email and password.

--- lib/screens/auth/Login_Screen.dart
+++ lib/screens/auth/Login_Screen.dart
@@ -8,11 +8,71 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _obscurePassword = true;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: SingleChildScrollView(
+      body: SafeArea(
+          child: SingleChildScrollView(
+        padding: const EdgeInsets.all(24.0),
+            child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 50,),
+                    const Center(
+                      child: Icon(Icons.restaurant_menu,
+                        size: 80,
+                        color: Colors.orange,
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    const Center(
+                      child: Text(
+                        'Welcome Back!',
+                        style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                    ),
 
+                    const SizedBox(height: 40),
+                    TextFormField(
+                      // controller: _emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        prefixIcon: Icon(Icons.email_outlined),
+                      ),
+                      validator: (value) =>
+                      value!.isEmpty ? "Enter your email" : null,
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      // controller: _passwordController,
+                      obscureText: _obscurePassword,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        prefixIcon: const Icon(Icons.lock_outline),
+                        suffixIcon: IconButton(
+                          icon: Icon(_obscurePassword
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                          onPressed: () {
+                            setState(() {
+                              _obscurePassword = !_obscurePassword;
+                            });
+                          },
+                        ),
+                      ),
+                      validator: (value) =>
+                      value!.isEmpty ? "Enter your password" : null,
+                    ),
+                  ],
+
+            )),
       )),
     );
   }
