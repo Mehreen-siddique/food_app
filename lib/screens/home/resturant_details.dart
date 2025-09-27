71b951a3b40a7c488d2735208b916a237d098cbf
Mehreen-Siddique <mehreensiddique50@gmail.com>
2025-09-27 17:16:26 +0500

    Add the information of restaurant.

--- lib/screens/home/resturant_details.dart
+++ lib/screens/home/resturant_details.dart
@@ -15,7 +15,22 @@ class _RestaurantDetailsScreenState extends State<RestaurantDetailsScreen> {
     ModalRoute.of(context)!.settings.arguments as RestaurantModel;
     return Scaffold(
       appBar: AppBar(title: Text(restaurant.name)),
-
+ body: Padding(padding: const EdgeInsets.all(16.0),
+ child: Column(
+   crossAxisAlignment: CrossAxisAlignment.start,
+   children: [
+     Text(restaurant.description,
+         style:
+         const TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
+     const SizedBox(height: 16),
+     Text("Delivery Time: ${restaurant.deliveryTime}"),
+     Text("Delivery Fee: \$${restaurant.deliveryFee.toStringAsFixed(2)}"),
+     const SizedBox(height: 20),
+     const Text("Menu (Demo):"),
+     const SizedBox(height: 10),
+   ],
+ ),
+ ),
     );
   }
 }
