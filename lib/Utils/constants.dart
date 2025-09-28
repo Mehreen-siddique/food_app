fac23d81454d1a9474968dfcdb3c29642b55507a
Mehreen-Siddique <mehreensiddique50@gmail.com>
2025-09-28 16:16:10 +0500

    constans screen with padding, border radius widgets defining.

---
+++ lib/Utils/constants.dart
@@ -0,0 +1,15 @@
+class AppConstants {
+  static const double defaultPadding = 16.0;
+  static const double smallPadding = 8.0;
+  static const double largePadding = 24.0;
+
+  static const double borderRadius = 12.0;
+  static const double smallBorderRadius = 8.0;
+  static const double largeBorderRadius = 20.0;
+
+  static const double defaultElevation = 4.0;
+  static const double smallElevation = 2.0;
+
+  static const Duration defaultAnimation = Duration(milliseconds: 300);
+  static const Duration fastAnimation = Duration(milliseconds: 150);
+}
